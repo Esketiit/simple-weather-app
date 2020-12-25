e4c9cef504d5a6e5f0c667b4b51ca9b6078c8f48
github username <heftygac@gmail.com>
2020-12-25 17:00:03 -0500

    added error handling

--- key.js
+++ key.js
@@ -1,5 +1,7 @@
-const API = "68feb0238fd5bf731167e86146b2004e"
+const weatherstack = "68feb0238fd5bf731167e86146b2004e"
+const maps = "pk.eyJ1Ijoib2xsaW1hbGUiLCJhIjoiY2tqMzR3bXdvMmRoYTJ5cXRrdWlrcXZmOSJ9.QlsVSol_WUqDNkOGUoqDAA"
 
 module.exports = {
-    "API": API
+    weatherstack: weatherstack,
+    maps: maps
 }
